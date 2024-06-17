dfdf10483f2fc98e1c3ca5a58867af44e611f54e
Felipe Jimenez <fsjimenez1@utpl.edu.ec>
2024-06-17 15:10:26 -0500

    COmmit

--- taller/deporteecuador/futbolec/models.py
+++ taller/deporteecuador/futbolec/models.py
@@ -1,53 +1,34 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Equipo(models.Model):
-
     nombre = models.CharField(max_length=30)
-    siglas = models.CharField(max_length=30)  # el campo puede                                                    
+    siglas = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
-    # modulos = models.ManyToManyField('Modulo', through='Matricula')
-
 
     def __str__(self):
-        return "%s - %s - %s" % (self.nombre,
-                self.siglas,
-                self.username)
-
+        return "%s - %s - %s" % (self.nombre, self.siglas, self.username)
 
 class Jugador(models.Model):
-
     nombre = models.CharField(max_length=30)
-    posicion = models.CharField(max_length=30)  # el campo puede                                                    
-    numero_camiseta = models.IntField()
-    sueldo = models.IntField()
-    equipo = models.ForeignKey(Equipo, related_name='equipos')
+    posicion = models.CharField(max_length=30)
+    numero_camiseta = models.IntegerField()
+    sueldo = models.IntegerField()
+    equipo = models.ForeignKey(Equipo, related_name='jugadores', on_delete=models.CASCADE)
 
     def __str__(self):
-        return "%s - %s - %d - %d" % (self.nombre,
-                self.posicion,
-                self.numero_camiseta,
-                self.sueldo)
-
+        return "%s - %s - %d - %d" % (self.nombre, self.posicion, self.numero_camiseta, self.sueldo)
 
 class Campeonato(models.Model):
-
     nombre_campeonato = models.CharField(max_length=30)
-    nombre_auspiciante = models.CharField(max_length=30)  # el campo puede                                                    
+    nombre_auspiciante = models.CharField(max_length=30)
 
     def __str__(self):
-        return "%s - %s" % (self.nombre_campeonato,
-                self.nombre_auspiciante)
-
+        return "%s - %s" % (self.nombre_campeonato, self.nombre_auspiciante)
 
 class CampeonatoEquipos(models.Model):
-    """
-    """
     anyo = models.CharField(max_length=30)
-    equipo = models.ForeignKey(Equipo, related_name='equiposC')                                             
-    campeonato = models.ForeignKey(Campeonato, related_name='campeonatos')       
+    equipo = models.ForeignKey(Equipo, related_name='campeonatos', on_delete=models.CASCADE)
+    campeonato = models.ForeignKey(Campeonato, related_name='equipos', on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s" % (self.anyo)

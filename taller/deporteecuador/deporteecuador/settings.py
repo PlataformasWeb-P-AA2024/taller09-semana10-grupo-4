24f1f0f6cc62f89028e1b4eda07b816a990e78ec
JoseUrdiales <jlurdiales1@utpl.edu.ec>
2024-06-18 20:48:00 -0500

    django con postgres

--- taller/deporteecuador/deporteecuador/settings.py
+++ taller/deporteecuador/deporteecuador/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'deporteecuador.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'futbol',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,12 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
